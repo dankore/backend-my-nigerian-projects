ed0a0416228060753aad0e30ce37de6891434ed2
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-10 22:41:18 -0500

    user registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,15 @@
-exports.apiRegister = (req, res) =>{
-  console.log(req.body)
-}
+const User = require('../models/userModel');
+
+exports.apiRegister = (req, res) => {
+  console.log(req.body);
+
+  let user = new User(req.body);
+  user
+    .register()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+};
---
+++ server/models/userModel.js
@@ -0,0 +1,21 @@
+const usersCollection = require('../../db').db().collection('users');
+
+let User = class user_ {
+  constructor(data) {
+    this.data = data;
+  }
+};
+
+User.prototype.register = function () {
+  return new Promise(async (resolve, reject) => {
+   usersCollection.insertOne(this.data)
+   .then(info=>{
+     console.log(info);
+     resolve("User created.")
+   })
+  });
+};
+
+
+
+module.exports = User;
