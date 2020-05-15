ab96ebd73cf8ba9d4f862ba337f6a5fd2d32f718
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-15 18:53:35 -0500

    Update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const server = express();
-const PortNumber = 8080;
 const apiRouter = require('./apiRouter');
 
 server.use(express.urlencoded({ extended: false }));
