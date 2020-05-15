a5068d01ca08aa7687f7e5635f322870fa0d2493
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-15 18:29:32 -0500

    Update

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
-const PORTNUMBER = process.env.PORTNUMBER || 8080;
+
 const mongodb = require("mongodb");
 
 mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
   module.exports = client;
+  const PORTNUMBER = process.env.PORTNUMBER;
   const server = require("./server/server");
   server.listen(PORTNUMBER, () => {
     console.log(`Listening on port number: ${PORTNUMBER}`);
