a91cd5901fc1c6375f5af418b778226bfb99c580
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-15 19:09:28 -0500

    Update

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ const mongodb = require("mongodb");
 
 mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
   module.exports = client;
-  const PORTNUMBER = process.env.PORTNUMBER;
+  const PORT = process.env.PORT || 8080;
   const server = require("./server/server");
-  server.listen(PORTNUMBER, () => {
-    console.log(`Listening on port number: ${PORTNUMBER}`);
+  server.listen(PORT, () => {
+    console.log(`Listening on port number: ${PORT}`);
   });
 });
