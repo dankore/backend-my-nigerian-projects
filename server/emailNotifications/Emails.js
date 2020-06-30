10528324afb1d6bcde6c7c70dd62866867ead391
Adamu M. Dankore <zimmazone@yahoo.com>
2020-06-30 07:32:24 -0500

    Email notification update

--- server/emailNotifications/Emails.js
+++ server/emailNotifications/Emails.js
@@ -1,5 +1,5 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 let Email = class email {
   constructor(from, to, subject, html) {
@@ -11,26 +11,26 @@ let Email = class email {
 };
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
   auth: {
-    user: "thebiddingapp@gmail.com",
-    pass: process.env.GMAILPW
-  }
+    user: 'thebiddingapp@gmail.com',
+    pass: process.env.GMAILPW,
+  },
 });
 
 transporter.verify((error, success) => {
   if (error) console.log(error);
-  else console.log("Server is ready to take email messages");
+  else console.log('Server is ready to take email messages');
 });
 Email.prototype.whoLoggedIn = attemptedUserFirstName => {
   const data = {
     from: '"The Bidding App" <thebiddingapp@gmail.com>',
-    to: "adamu.dankore@gmail.com",
-    subject: `Login from ${attemptedUserFirstName}`
+    to: 'adamu.dankore@gmail.com',
+    subject: `Login from ${attemptedUserFirstName}`,
   };
   transporter.sendMail(data, (error, info) => {
     if (error) console.log(error);
-    else console.log("Who Logs in Email Sent: " + info.response);
+    else console.log('Who Logs in Email Sent: ' + info.response);
   });
 };
 
@@ -39,28 +39,11 @@ Email.prototype.sendResetPasswordToken = (email, firstName, url, token) => {
     from: '"The Bidding App" <thebiddingapp@gmail.com>',
     to: email,
     subject: `${firstName}, Reset Your Password - The Bidding App`,
-    html:
-      `Hello ${firstName},` +
-      "<br><br>" +
-      "Please click on the following link to complete the process:\n" +
-      '<a href="' +
-      url +
-      "/reset-password/" +
-      token +
-      '">Reset your password</a><br>' +
-      "OR" +
-      "<br>" +
-      "Paste the below URL into your browser to complete the process:" +
-      "<br>" +
-      url +
-      "/reset-password/" +
-      token +
-      "<br><br>" +
-      "If you did not request this, please ignore this email and your password will remain unchanged.\n"
+    html: `Hello ${firstName},` + '<br><br>' + 'Please click on the following link to complete the process:\n' + '<a href="' + url + '/reset-password/' + token + '">Reset your password</a><br>' + 'OR' + '<br>' + 'Paste the below URL into your browser to complete the process:' + '<br>' + url + '/reset-password/' + token + '<br><br>' + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
   };
   transporter.sendMail(data, (err, info) => {
     if (err) console.log(err);
-    else console.log("Reset Password Token Sent Via Email: " + info.response);
+    else console.log('Reset Password Token Sent Via Email: ' + info.response);
   });
 };
 
@@ -68,11 +51,9 @@ Email.prototype.projectSuccessfullyCreated = projectData => {
   const data = {
     from: '"The Bidding App" <thebiddingapp@gmail.com>',
     to: projectData.email,
-    subject: `Your project, ${
-      projectData.title
-    }, was created - The Bidding App`,
+    subject: `Your project, ${projectData.title}, was created - The Bidding App`,
     html: `<div style="background:#ffffff;font-family:Arial,Helvetica,sans-serif;font-size:1em">
-    <table id="m_-1754539424995552259backgroundTable" width="100%" cellspacing="0" cellpadding="0" border="0" style="background:#ffffff">
+    <table width="100%" cellspacing="0" cellpadding="0" border="0" style="background:#ffffff">
        <tbody>
           <tr>
              <td align="center" valign="top" style="background:#ffffff" width="100%">
@@ -121,9 +102,7 @@ Email.prototype.projectSuccessfullyCreated = projectData => {
                                               </tr>
                                               <tr>
                                                  <td style="font-family:Helvetica,Geneva,Tahoma,Verdana,sans-serif;font-size:16px;padding:0px 30px 45px" align="left">
-                                                    <a href="https://bidding.netlify.app/project/${
-                                                      projectData._id
-                                                    }" style="background:#0060df;color:#fff;text-decoration:none;border:14px solid #0060df;border-left-width:50px;border-right-width:50px;display:inline-block" target="_blank">
+                                                    <a href="https://bidding.netlify.app/project/${projectData._id}" style="background:#0060df;color:#fff;text-decoration:none;border:14px solid #0060df;border-left-width:50px;border-right-width:50px;display:inline-block" target="_blank">
                                                     View on the Bidding App
                                                     </a>  
                                                  </td>
@@ -168,12 +147,11 @@ Email.prototype.projectSuccessfullyCreated = projectData => {
        </tbody>
     </table>
     </td></tr></tbody></table>
- </div>`
+ </div>`,
   };
   transporter.sendMail(data, (err, info) => {
     if (err) console.log(err);
-    else
-      console.log("Project Creation Success Sent Via Email: " + info.response);
+    else console.log('Project Creation Success Sent Via Email: ' + info.response);
   });
 };
 
