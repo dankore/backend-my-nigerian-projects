2d8d0abe55f40acec74530b06efecec06f910721
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-13 05:21:15 -0500

    update file

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 const apiRouter = require('./apiRouter');
-const morgan = require('morgan');
 
-server.use(morgan('tiny'));
 server.use(cors());
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
