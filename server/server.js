46a95c16a467c1e06d38ac136456acb559c1e99d
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-12 23:33:03 -0500

    morgan and hemlmet

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 const apiRouter = require('./apiRouter');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
+server.use(helmet());
+server.use(morgan('tiny'));
 server.use(cors());
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
