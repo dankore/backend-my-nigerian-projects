ad0db9759d6be0f46a8f59d15c05936830ae8be7
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2021-08-29 15:11:48 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const apiRouter = require('./apiRouter');
 // });
 
 const corsOptions = {
-  origin: 'https://api.mynigerianprojects.com',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
