8549a9a8b68378ffed5174cc5602b2500abc5e15
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2021-08-29 15:03:55 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -3,13 +3,18 @@ const server = express();
 const cors = require('cors');
 const apiRouter = require('./apiRouter');
 
-server.use(function (_, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// server.use(function (_, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
-// server.use(cors());
+const corsOptions = {
+  origin: 'https://api.mynigerianprojects.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+server.use(cors(corsOptions));
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use('/', apiRouter);
