a351e6b8057efacb85988e964cac040175f9eb4a
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2021-08-29 14:55:26 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,13 @@ const server = express();
 const cors = require('cors');
 const apiRouter = require('./apiRouter');
 
-server.use(cors());
+server.use(function (_, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// server.use(cors());
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use('/', apiRouter);
